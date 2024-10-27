34a842a995da2e08d9341cc000ec6129dc9c9841
nishad <nishadalichenadan@gmail.com>
2024-10-27 13:42:33 +0530

    form validation completed

--- src/Api/axiosInstance.jsx
+++ src/Api/axiosInstance.jsx
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const axiosInstance  = axios.create({
+//     baseURL : "http://localhost:3000",
+//     withCredentials : true
+// })
+
+// export default axiosInstance
 import axios from "axios";
 
 const axiosInstance  = axios.create({
-    baseURL : "http://localhost:3000",
+    baseURL : "https://invoicegenerator.up.railway.app",
     withCredentials : true
 })
 
